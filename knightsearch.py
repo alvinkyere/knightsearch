f17d5ee9584c9f7900d181fcb65140818178939a
Alvin Kyere <alvinkyere2@gmail.com>
2026-03-03 16:57:01 -0500

    requirements and others for render deployment

--- knightsearch.py
+++ knightsearch.py
@@ -33,4 +33,4 @@ def search():#get the query from the requests
         return render_template('results.html', urls = urls, query = query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
